41b3f7a53d2e01b1e03ba213c41a08e7296e3a2f
Jernej Vivod <je.vivod@gmail.com>
2018-09-05 20:19:51 +0200

    Add comments to cheatsheet/introduction

--- warmup/introduction_to_R.R
+++ warmup/introduction_to_R.R
@@ -9,12 +9,12 @@
 
 
 # assignment operators
-x = 945
-y <- sin(0.47)^2 * sqrt(5)
+x = 945 					# discouraged
+y <- sin(0.47)^2 * sqrt(5) 	# encouraged
 y^2 -> z
 
 
-# to inspect the value of a variable simply type its name
+# to inspect the value of a variable simply type its name (similar to MATLAB or Python)
 x
 y
 z
@@ -34,28 +34,26 @@ rm(list=ls())
 #
 
 # creating vectors
-v <- c(14,7,23.5,76.2)
+v <- c(14,7,23.5,76.2) 						# c is a function that combines objects into a vector
 v
 
 # generating a regular sequence of numbers
-v <- 1:10
+v <- 1:10 									# similar to MATLAB
 v
 
-v <- seq(from=5, to=10, by=2)
+v <- seq(from=5, to=10, by=2) 				# Function that returns a sequece
 v
 
-w <- rep(v, times = 2)
+w <- rep(v, times = 2) 						# Repeat vector v 2 times
 w
 
 
 # scalars are vectors with a single element
 w <- 45.0
 
-# vectors can be created using other vectors
+# vectors can be created using other vectors (similar to MATLAB)
 z <- c(v, 2.5, w)
 z
-
-
 
 
 #
@@ -81,7 +79,7 @@ order(v)
 mode(v)
 
 # logical vector - has logical constants as elements 
-b <- c(TRUE, FALSE, F, T)
+b <- c(TRUE, FALSE, F, T) 				# F and T are aliases for TRUE and FALSE
 b
 mode(b)
 
@@ -92,7 +90,7 @@ mode(x)
 
 # string vector - has strings as elements
 s <- c("character", "logical", "numeric", "complex")
-mode(s)
+mode(s) # type of file stored in vector
 
 
 # type coercion (all elements must be of the same type)
@@ -108,7 +106,7 @@ x
 v1 <- c(10,20,30,40)
 v2 <- 1:4
 v1 + v2
-v1 * v2
+v1 * v2 	# Hadamard/Shur product
 
 
 # functions operate directly on each element of a vector
@@ -120,7 +118,7 @@ log2(v1)
 # the recycling rule (if lengths are different the elements of the shorter vector are repeated)
 v1 * 10
 v1 + 1
-v1 + c(100, 200)
+v1 + c(100, 200) # Add 100 to first element in v1, 200 to second element in v1, 100 to third element in v1 and so on.
 
 
 
@@ -153,29 +151,26 @@ x[-(1:3)]
 x > 0
 
 # logical vector indexing
-x[x>0]
+x[x>0]						# Positive elements
 x[x <= -20 | x > 50]
 x[x > 40 & x < 100]
 
 # equality operator is ==
 # inequality operator is !=
 
-
 # the which() function returns indices corresponding to constants TRUE
-which(x > 0)
-
-
+which(x > 0)	# Returns indices of elements that are greater than 0.
 
 # character string index vector
-point <- c(4.7, 3.6, 2.5)
+point <- c(4.7, 3.6, 2.5) 			# "Records/dictionaries"
 names(point) <- c('x', 'y', 'z')
 point
 
-point['x']
+point['x'] 							# Accessing element by name/key
 point[c('x','z')]
 
 # empty indices
-point[] <- 0
+point[] <- 0 						# Set all indices to 0.
 point
 
 # not the same as
@@ -193,26 +188,27 @@ x <- c("a", "b", "c", "d")
 x[2] <- "BBBBB"
 x
 
+# Replacing a subvector
 x[c(1,3)] <- c("AAAAA", "CCCCC")
 x
 
-# adding new element
+# adding new element (append)
 x[length(x)+1] = "EEEEE"
 x
 
 # what happens if we do not define all elements in the vector?
-x[10] <- "FFFFF"
+x[10] <- "FFFFF" 		# Intermediate values are filled with NA
 x
 
 # which elements are not defined
-is.na(x)
+is.na(x) 	# Returns a vector of boolean values which is TRUE where there are NAs in the original vector.
 
 
 # removing elements
-x <- x[-c(1,3)]
+x <- x[-c(1,3)]			# All elements except the first and third.
 x
 
-x <- c(x[2],x[3])
+x <- c(x[2],x[3]) 		# Only the second and third element.
 x
 
 
@@ -230,22 +226,22 @@ gender <- factor(gender)
 gender
 
 # argument "levels" defines all possible elements' values
-dir <- factor(c('left','left','up'), levels = c('left','right','up','down'))
+dir <- factor(c('left','left','up'), levels = c('left','right','up','down')) 	# Useful when all possible values not guaranteed to be present.
 dir
 
 # all possible elements' values
-levels(dir)
+levels(dir) 
 
 # if no match is found
-dir[1] <- "diagonal"
+dir[1] <- "diagonal" # Adds NA
 dir
 
 # valid assignment
-dir[1] <- "down"
+dir[1] <- "down" 	# adds "down" to vector
 dir
 
 # frequency tables for factors 
-table(gender)
+table(gender)	# Count how many times each value appears.
 table(dir)
 
 
@@ -255,11 +251,11 @@ table(dir)
 #
 
 # creating a list
-student <- list(id=12345,name="Marko",marks=c(10,9,10,9,8,10))
+student <- list(id=12345,name="Marko",marks=c(10,9,10,9,8,10)) 	# A "record"
 student
 
 # extracting elements of a list (using named components)
-student$id
+student$id 				# projection
 student$name
 student$marks
 
@@ -269,13 +265,21 @@ student[[2]]
 student[[3]]
 
 # extending lists
-student$parents <- c("Ana", "Tomaz")
+student$parents <- c("Ana", "Tomaz")  # Add another field.
 student
 
 
 #
 # Data frames
 #
+
+# R: dataframe is a quasi-builtin data type in R.
+# Technically, it is not a primitive; the language definition mentions "Data frame objects" only in passing:
+# "A data frame is a list of vectors, factors, and/or matrices all having the same length (number of rows in the case of matrices).
+# In addition, a data frame generally has a names attribute labeling the variables and a row.names attribute for labeling the cases."
+# Vectors, lists, and "factors" are primitive; matrices and dataframes are not. But in practice the dataframe is central to R.
+# A web search will produce many tutorials on working with dataframes, e.g.
+
  
 # creating a data frame
 height <- c(179, 185, 183, 172, 174, 185, 193, 169, 173, 168)
@@ -287,29 +291,29 @@ df <- data.frame(gender, height, weight, student)
 df
 
 # some important functions
-summary(df)
-names(df)
-nrow(df)
-ncol(df)
+summary(df) 					# Get summary of data.
+names(df) 						# Get names of vectors in data frame.
+nrow(df) 						# Get number of rows in data frame.
+ncol(df)						# Get number of columns in data frame.
 
 # accessing elements of data frames
-df[5,]
-df[1:5,]
-df[,1]
-df[,c(1,3,4)]
-df[1,3]
-df[1,-3]
+df[5,]			# 5th row
+df[1:5,] 		# first 5 rows
+df[,1] 			# first vector in data frame
+df[,c(1,3,4)] 	# first, third and fourth vector in data frame
+df[1,3] 		# first row second column
+df[1,-3] 		# first row and all except the third column
 
-df$height
+df$height 		# select vectors by name
 
-df[df$height < 180,]
-df[df$gender == "m",]
-
+df[df$height < 180,] 	# Select every column of rows where height is less than 180
+df[df$gender == "m",]	# Select every column of rows where value of gender vector is "m"
 
 # adding columns to a data frame
 df <- cbind(df, age = c(20, 21, 30, 25, 27, 19, 24, 27, 28, 24))
 df
 
+# another way to add columns to a data frame
 df$name = c("Joan","Tom","John","Mike","Anna","Bill","Tina","Beth","Steve","Kim")
 df
 
